4fc8b1c7e2b723e49f5b16b9c9b14bc0ef501aa3
Kirilon01888 <petrisevkirill144@gmail.com>
2024-03-12 07:31:58 +0300

    a bit

--- learn.php
+++ learn.php
@@ -170,114 +170,119 @@ class Converter
         $start = $range[0];
         $end = $range[1];
         $s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        for((int)$start[1]; $start <= )
-        $baseTemplate = [
-            '_variable_->getActiveSheet()->getStyle($cells)->applyFromArray(',
-            'alignment' => "",
-            'font' => "",
-            'fill' => "",
-            'borders' => ""
-        ];
-        $baseTemplate['alignment'] = $this->alignment;
-        $baseTemplate['font'] = $this->font;
-        $myHash = '';
-        $arrOfHashAndCell = [];
-        //alignment
-        $c = self::$fileReader->getStyle('B1')->getAlignment()->getHorizontal();
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["alignment"] = str_replace('<HOR>', $this->arrAlignmentHorizontal[$c], $baseTemplate['alignment']);
-        $c = self::$fileReader->getStyle('B1')->getAlignment()->getVertical();
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["alignment"] = str_replace('<VER>', $this->arrAlignmentVertical[$c], $baseTemplate["alignment"]);
-        $c = self::$fileReader->getStyle('B1')->getAlignment()->getTextRotation();
-        $baseTemplate["alignment"] = str_replace('<R>', $c, $baseTemplate["alignment"]);
-        $c = self::$fileReader->getStyle('B1')->getAlignment()->getWrapText() ? 'true' : 'false';
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["alignment"] = str_replace('<WRAP>', $c, $baseTemplate["alignment"]);
-        $c = self::$fileReader->getRowDimension('1')->getRowHeight();
-        array_push($afterRes, '_variable_->getRowDimension()->setRowHeight('.$c.');');
-        $c = self::$fileReader->getColumnDimension('B')->getWidth();
-        array_push($afterRes, '_variable_->getColumnDimension()->setWidth('.$c.');');
-        $c = self::$fileReader->getCell('B1')->getValue();
-        array_push($afterRes, '_variable_->getCell()->setValue('.$c.');');
-
-        //font
-        $c = self::$fileReader->getStyle('B1')->getFont()->getSize();
-        $baseTemplate["alignment"] = str_replace('<SIZE>', $c, $baseTemplate["alignment"]);
-        $c = self::$fileReader->getStyle('B1')->getFont()->getName();
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["alignment"] = str_replace('<NAME>', $c, $baseTemplate["alignment"]);
-        $c = self::$fileReader->getStyle('B1')->getFont()->getColor()->getARGB();
-        $baseTemplate["font"] = str_replace('<ARGB>', $c, $baseTemplate["font"]);
-        $c = self::$fileReader->getStyle('B1')->getFont()->getSubscript() ? 'true' : 'false';
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["font"] = str_replace('<SUB>', $c, $baseTemplate["font"]);
-        $c = self::$fileReader->getStyle('B1')->getFont()->getSuperscript() ? 'true' : 'false';
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["font"] = str_replace('<SUP>', $c, $baseTemplate["font"]);
-        $c = self::$fileReader->getStyle('B1')->getFont()->getUnderline();
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["font"] = str_replace('<UND>', $this->arrUnderLine[$c], $baseTemplate["font"]);
-        $c = self::$fileReader->getStyle('B1')->getFont()->getBold() ? 'true' : 'false';
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["font"] = str_replace('<BOLD>', $c, $baseTemplate["font"]);
-        $c = self::$fileReader->getStyle('B1')->getFont()->getItalic() ? 'true' : 'false';
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["font"] = str_replace('<ITAL>', $c, $baseTemplate["font"]);
-        $c = self::$fileReader->getStyle('B1')->getFont()->getStrikethrough() ? 'true' : 'false';
-        $myHash .= substr($c, 0, 3);
-        $baseTemplate["font"] = str_replace('<STK>', $c, $baseTemplate["font"]);
-
-        //fill
-        $c = self::$fileReader->getStyle('B1')->getFill()->getFillType();
-        echo $c == 'path';
-        if ($c == 'linear' or $c == 'path') {
-            $baseTemplate['fill'] = $this->fill;
-            $myHash .= substr($c, 0, 3);
-            $baseTemplate["fill"] = str_replace('<IFGRAD>', $this->fillGrad, $baseTemplate["fill"]);
-            $baseTemplate["fill"] = str_replace('<FILL>', $this->arrFills[$c], $baseTemplate["fill"]);
-            $myHash .= substr($c, 0, 3);
-            $c = self::$fileReader->getStyle('B1')->getFill()->getRotation();
-            $baseTemplate["fill"] = str_replace('<R>', $c, $baseTemplate["fill"]);
-            $c = self::$fileReader->getStyle('B1')->getFill()->getStartColor()->getARGB();
-            $baseTemplate["fill"] = str_replace('<COLS>', $c, $baseTemplate["fill"]);
-            $c = self::$fileReader->getStyle('B1')->getFill()->getEndColor()->getARGB();
-            $baseTemplate["fill"] = str_replace('<COLE>', $c, $baseTemplate["fill"]);
-        } else {
-            $baseTemplate["fill"] = str_replace('<FILL>', $this->arrFills[$c], $baseTemplate["fill"]);
-        }
-
-        //borders
-        $c = self::$fileReader->getStyle('B1')->getBorders()->getLeft()->getBorderStyle();
-        if ($c != 'none') {
-            $myHash .= substr($c, 0, 3);
-            $baseTemplate['borders'] = $this->bord['left'];
-            $baseTemplate['borders']['left'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['left']);
-        }
-
-        $c = self::$fileReader->getStyle('B1')->getBorders()->getRight()->getBorderStyle();
-        if ($c != 'none') {
-            $myHash .= substr($c, 0, 3);
-            $baseTemplate['borders'] = $this->bord['right'];
-            $baseTemplate['borders']['right'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['right']);
-        }
-        $c = self::$fileReader->getStyle('B1')->getBorders()->getTop()->getBorderStyle();
-        if ($c != 'none') {
-            $myHash .= substr($c, 0, 3);
-            $baseTemplate['borders'] = $this->bord['top'];
-            $baseTemplate['borders']['top'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['top']);
-        }
-        $c = self::$fileReader->getStyle('B1')->getBorders()->getBottom()->getBorderStyle();
-        if ($c != 'none') {
-            $myHash .= substr($c, 0, 3);
-            $baseTemplate['borders'] = $this->bord['bottom'];
-            $baseTemplate['borders']['bottom'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['bottom']);
-        }
-        $c = self::$fileReader->getStyle('B1')->getBorders()->getDiagonal()->getBorderStyle();
-        if ($c != 'none') {
-            $myHash .= substr($c, 0, 3);
-            $baseTemplate['borders'] = $this->bord['diagonal'];
-            $baseTemplate['borders']['diagonal'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['diagonal']);
+        
+        for($startWord = strpos($s, $start[0]); $startWord <= strpos($s, $end[0]); $start++){
+            $word = $s[$startWord];
+            for($startRow = (int)$start[1]; $startRow < (int)$end[1]; $startRow++){
+                $baseTemplate = [
+                    '_variable_->getActiveSheet()->getStyle($cells)->applyFromArray(',
+                    'alignment' => "",
+                    'font' => "",
+                    'fill' => "",
+                    'borders' => ""
+                ];
+                $baseTemplate['alignment'] = $this->alignment;
+                $baseTemplate['font'] = $this->font;
+                $myHash = '';
+                $arrOfHashAndCell = [];
+                //alignment
+                $c = self::$fileReader->getStyle($word.$startRow)->getAlignment()->getHorizontal();
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["alignment"] = str_replace('<HOR>', $this->arrAlignmentHorizontal[$c], $baseTemplate['alignment']);
+                $c = self::$fileReader->getStyle($word.$startRow)->getAlignment()->getVertical();
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["alignment"] = str_replace('<VER>', $this->arrAlignmentVertical[$c], $baseTemplate["alignment"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getAlignment()->getTextRotation();
+                $baseTemplate["alignment"] = str_replace('<R>', $c, $baseTemplate["alignment"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getAlignment()->getWrapText() ? 'true' : 'false';
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["alignment"] = str_replace('<WRAP>', $c, $baseTemplate["alignment"]);
+                $c = self::$fileReader->getRowDimension('1')->getRowHeight();
+                array_push($afterRes, '_variable_->getRowDimension("'.$startRow.'")->setRowHeight('.$c.');');
+                $c = self::$fileReader->getColumnDimension('B')->getWidth();
+                array_push($afterRes, '_variable_->getColumnDimension()->setWidth('.$c.');');
+                $c = self::$fileReader->getCell($word.$startRow)->getValue();
+                array_push($afterRes, '_variable_->getCell()->setValue('.$c.');');
+        
+                //font
+                $c = self::$fileReader->getStyle($word.$startRow)->getFont()->getSize();
+                $baseTemplate["alignment"] = str_replace('<SIZE>', $c, $baseTemplate["alignment"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getFont()->getName();
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["alignment"] = str_replace('<NAME>', $c, $baseTemplate["alignment"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getFont()->getColor()->getARGB();
+                $baseTemplate["font"] = str_replace('<ARGB>', $c, $baseTemplate["font"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getFont()->getSubscript() ? 'true' : 'false';
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["font"] = str_replace('<SUB>', $c, $baseTemplate["font"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getFont()->getSuperscript() ? 'true' : 'false';
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["font"] = str_replace('<SUP>', $c, $baseTemplate["font"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getFont()->getUnderline();
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["font"] = str_replace('<UND>', $this->arrUnderLine[$c], $baseTemplate["font"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getFont()->getBold() ? 'true' : 'false';
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["font"] = str_replace('<BOLD>', $c, $baseTemplate["font"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getFont()->getItalic() ? 'true' : 'false';
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["font"] = str_replace('<ITAL>', $c, $baseTemplate["font"]);
+                $c = self::$fileReader->getStyle($word.$startRow)->getFont()->getStrikethrough() ? 'true' : 'false';
+                $myHash .= substr($c, 0, 3);
+                $baseTemplate["font"] = str_replace('<STK>', $c, $baseTemplate["font"]);
+        
+                //fill
+                $c = self::$fileReader->getStyle($word.$startRow)->getFill()->getFillType();
+                echo $c == 'path';
+                if ($c == 'linear' or $c == 'path') {
+                    $baseTemplate['fill'] = $this->fill;
+                    $myHash .= substr($c, 0, 3);
+                    $baseTemplate["fill"] = str_replace('<IFGRAD>', $this->fillGrad, $baseTemplate["fill"]);
+                    $baseTemplate["fill"] = str_replace('<FILL>', $this->arrFills[$c], $baseTemplate["fill"]);
+                    $myHash .= substr($c, 0, 3);
+                    $c = self::$fileReader->getStyle($word.$startRow)->getFill()->getRotation();
+                    $baseTemplate["fill"] = str_replace('<R>', $c, $baseTemplate["fill"]);
+                    $c = self::$fileReader->getStyle($word.$startRow)->getFill()->getStartColor()->getARGB();
+                    $baseTemplate["fill"] = str_replace('<COLS>', $c, $baseTemplate["fill"]);
+                    $c = self::$fileReader->getStyle($word.$startRow)->getFill()->getEndColor()->getARGB();
+                    $baseTemplate["fill"] = str_replace('<COLE>', $c, $baseTemplate["fill"]);
+                } else {
+                    $baseTemplate["fill"] = str_replace('<FILL>', $this->arrFills[$c], $baseTemplate["fill"]);
+                }
+        
+                //borders
+                $c = self::$fileReader->getStyle($word.$startRow)->getBorders()->getLeft()->getBorderStyle();
+                if ($c != 'none') {
+                    $myHash .= substr($c, 0, 3);
+                    $baseTemplate['borders'] = $this->bord['left'];
+                    $baseTemplate['borders']['left'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['left']);
+                }
+        
+                $c = self::$fileReader->getStyle($word.$startRow)->getBorders()->getRight()->getBorderStyle();
+                if ($c != 'none') {
+                    $myHash .= substr($c, 0, 3);
+                    $baseTemplate['borders'] = $this->bord['right'];
+                    $baseTemplate['borders']['right'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['right']);
+                }
+                $c = self::$fileReader->getStyle($word.$startRow)->getBorders()->getTop()->getBorderStyle();
+                if ($c != 'none') {
+                    $myHash .= substr($c, 0, 3);
+                    $baseTemplate['borders'] = $this->bord['top'];
+                    $baseTemplate['borders']['top'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['top']);
+                }
+                $c = self::$fileReader->getStyle($word.$startRow)->getBorders()->getBottom()->getBorderStyle();
+                if ($c != 'none') {
+                    $myHash .= substr($c, 0, 3);
+                    $baseTemplate['borders'] = $this->bord['bottom'];
+                    $baseTemplate['borders']['bottom'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['bottom']);
+                }
+                $c = self::$fileReader->getStyle($word.$startRow)->getBorders()->getDiagonal()->getBorderStyle();
+                if ($c != 'none') {
+                    $myHash .= substr($c, 0, 3);
+                    $baseTemplate['borders'] = $this->bord['diagonal'];
+                    $baseTemplate['borders']['diagonal'] = str_replace('<BRD>', $this->arrBorders[$c], $baseTemplate['borders']['diagonal']);
+                }
+            }
         }
     }
 }
